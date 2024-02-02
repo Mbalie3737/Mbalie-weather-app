59be1dd5d30c47f4bfc100ec0ce66ccc56e23706
Mbalie3737 <mbaliletsoara31@gmail.com>
2024-02-02 16:36:48 +0200

    added js file and injected basic js to form

---
+++ index.js
@@ -0,0 +1,11 @@
+function searchButton(event) {
+    event.preventDefault();
+    let searchInputElement = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#weather-city");
+    
+
+}
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchButton);
