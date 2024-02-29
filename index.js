2681ae7aa68381abcf567b0bdf10d10b3ec72e57
Mbalie3737 <mbaliletsoara31@gmail.com>
2024-02-29 15:40:00 +0200

    updated js

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ function searchButton(event) {
     event.preventDefault();
     let searchInputElement = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#weather-city");
-    
+    cityElement.innerHTML = searchInputElement.value;
 
 }
 
-
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchButton);
+searchFormElement.addEventListener("search",searchButton);
